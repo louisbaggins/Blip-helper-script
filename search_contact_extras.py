5e18cf2da97cf912ff7d78e403fe687b575fa161
Luis Correa <luisc@take.net>
2021-03-12 10:17:50 -0300

    Add service to add search contact per extras property

--- search_contact_extras.py
+++ search_contact_extras.py
@@ -1,22 +1,23 @@
 from blip_session import BlipSession
 
+AUTH_KEY = '{{bot_key}}'
+SEARCHED_VALUE = '{{searched_value}}'
+bs = BlipSession(AUTH_KEY, '{{organization}}') #Organization is optional#
+searched_key = '{{searched_key}}'
+
 def get_contact_crb(skip, take):
     return {
         "method" : "get",
         "uri" : f"/contacts?$skip={skip}&$take={take}"
     }
 
-AUTH_KEY = '{{bot_auth_key}}'
-SEARCHED_VALUE = '{{searched_value}}'
-bs = BlipSession(AUTH_KEY)
-
-response = bs.force_command(get_contact_crb(0, 100))
 skip = 0
 take = 100
+response = bs.force_command(get_contact_crb(skip, take))
 contact_found = False
 while skip < response['resource']['total']:
     for contact in response['resource']['items']:
-        if 'extras' in contact and 'CPF' in contact['extras'] and contact['extras']['CPF'] == SEARCHED_VALUE:
+        if 'extras' in contact and searched_key in contact['extras'] and contact['extras'][searched_key] == SEARCHED_VALUE:
             print(f"Valor encontrado em {contact['identity']}")
             contact_found = True
     if contact_found:
